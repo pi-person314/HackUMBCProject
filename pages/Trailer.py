dc5be6c4875f011f43483f2b154d95fea76d7f97
Victor Liu <victorliu5000@gmail.com>
2023-09-23 15:02:41 -0400

    new trailers

--- pages/Trailer.py
+++ pages/Trailer.py
@@ -1,2 +1,11 @@
 import streamlit as st
+import pandas as pd
 
+st.header('Trailers')
+title = st.text_input('Move title', 'Input movie title')
+st.write('Your movie is: ', title)
+
+video_file = open('"C:\Users\victo\Downloads\aacdaebc-d2c5-41dc-bb38-c481f55b235b.mp4"', 'rb')
+video_bytes = video_file.read()
+
+st.video(video_bytes)
