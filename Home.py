ee828c23b6e636a6bebd128c0ebcd9a4ce86087e
Victor Liu <victorliu5000@gmail.com>
2023-09-23 13:52:42 -0400

    asgdbaf

--- Home.py
+++ Home.py
@@ -1,5 +1,13 @@
 import streamlit as st
 import pandas as pd
+
+#Styling
+primaryColor="#F63366"
+backgroundColor="#FFFFFF"
+secondaryBackgroundColor="#F0F2F6"
+textColor="#262730"
+font="sans serif"
+
 df = pd.DataFrame({
   'first column': [1, 2, 3, 4],
   'second column': [10, 20, 30, 40]
