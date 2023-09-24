31f4d136e88ce36ec798c1d8835ceef5067a3573
Vinay <vinay7.karthik@gmail.com>
2023-09-24 02:27:03 -0400

    hi mr pi person

--- Dashboard.py
+++ Dashboard.py
@@ -6,9 +6,12 @@ stock_datasets = os.listdir("stocks")
 st.header("Dashboard")
 ticker = st.text_input("Enter a stock ticker")
 for dataset in stock_datasets:
-    path = "stocks\\" + dataset
-    print(path)
+    path = os.path.join("stocks", dataset)
     df = pd.read_csv(path)
     dataset_ticker = dataset.split(".")[0]
+    
     if dataset_ticker == ticker:
         st.write(df["Close"].head())
+
+        # Plotting the 'Close' column
+        st.line_chart(df.set_index('Date')["Close"], use_container_width=True)
