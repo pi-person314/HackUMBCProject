7fb944fd87c8cb280652deb8af56f658f59db3c6
pi-person314 <jadenmengl@gmail.com>
2023-09-24 02:12:30 -0400

    we can access data!!!

--- Dashboard.py
+++ Dashboard.py
@@ -1,13 +1,15 @@
 import os
 import pandas as pd
 import streamlit as st
+from path import Path
 
 stock_datasets = os.listdir(r'C:\Users\piper\OneDrive\Desktop\web-app-dev\HackUMBCProject\stocks')
 st.header("Dashboard")
 ticker = st.text_input("Enter a stock ticker")
 for dataset in stock_datasets:
-    path = "C:\Users\piper\OneDrive\Desktop\web-app-dev\HackUMBCProject\stocks\\" + dataset
-    df = pd.read_csv(dataset)
+    path = "stocks\\" + dataset
+    print(path)
+    df = pd.read_csv(path)
     dataset_ticker = dataset.split(".")[0]
     if dataset_ticker == ticker:
         st.write(df["Close"].head())
