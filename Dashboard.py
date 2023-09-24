0636fade3bda403225a1eb574c70b638b9949729
Victor Liu <victorliu5000@gmail.com>
2023-09-24 04:41:52 -0400

    qegrefsd

--- Dashboard.py
+++ Dashboard.py
@@ -1,1 +1,26 @@
+import os
+import pandas as pd
 import streamlit as st
+import plotly.graph_objects as go
+import matplotlib.pyplot as plt
+
+
+
+stock_datasets = os.listdir("spstocks")
+st.header("Search for Stocks")
+ticker = st.text_input("Enter a stock ticker in the S&P 500")
+
+for dataset in stock_datasets:
+    path = os.path.join("spstocks", dataset)
+    df = pd.read_csv(path)
+    dataset_ticker = dataset.split("_")[0]
+    
+    if dataset_ticker == ticker:
+        st.subheader(f"Closing Prices for {ticker}")
+        st.line_chart(df.set_index('date')["close"], use_container_width=True)
+        st.subheader(f"Volume Distribution for {ticker}")
+        st.bar_chart(df.set_index('date')["volume"], use_container_width=True)
+        st.subheader(f"Candlestick Chart for {ticker}")
+        fig = go.Figure(data=[go.Candlestick(x=df['date'], open=df['open'], high=df['high'], low=df['low'], close=df['close'])])
+        st.plotly_chart(fig, use_container_width=True)
+        break
