f48169b637b0983bdd0c1d34a1ce8118d206c034
Vinay <vinay7.karthik@gmail.com>
2023-09-24 02:53:31 -0400

    search for stonks

--- Dashboard.py
+++ Dashboard.py
@@ -1,17 +1,1 @@
-import os
-import pandas as pd
 import streamlit as st
-
-stock_datasets = os.listdir("spstocks")
-st.header("Dashboard")
-ticker = st.text_input("Enter a stock ticker")
-for dataset in stock_datasets:
-    path = os.path.join("spstocks", dataset)
-    df = pd.read_csv(path)
-    dataset_ticker = dataset.split("_")[0]
-    
-    if dataset_ticker == ticker:
-        st.write(df["close"].head())
-
-        # Plotting the 'Close' column
-        st.line_chart(df.set_index('date')["close"], use_container_width=True)
--- pages/Search.py
+++ pages/Search.py
@@ -1,1 +1,36 @@
+import os
+import pandas as pd
 import streamlit as st
+import plotly.graph_objects as go
+import matplotlib.pyplot as plt
+
+stock_datasets = os.listdir("spstocks")
+st.header("Search for Stocks")
+ticker = st.text_input("Enter a stock ticker")
+
+for dataset in stock_datasets:
+    path = os.path.join("spstocks", dataset)
+    df = pd.read_csv(path)
+    dataset_ticker = dataset.split("_")[0]
+    
+    if dataset_ticker == ticker:
+        st.write(df["close"].head())
+
+        # 1. Line chart for Closing Prices
+        st.subheader(f"Closing Prices for {ticker}")
+        st.line_chart(df.set_index('date')["close"], use_container_width=True)
+        
+        # 2. Histogram for Volume Distribution
+        st.subheader(f"Volume Distribution for {ticker}")
+        st.bar_chart(df.set_index('date')["volume"], use_container_width=True)
+
+        # 3. Candlestick plot
+        st.subheader(f"Candlestick Chart for {ticker}")
+        fig = go.Figure(data=[go.Candlestick(x=df['date'],
+                                             open=df['open'],
+                                             high=df['high'],
+                                             low=df['low'],
+                                             close=df['close'])])
+        st.plotly_chart(fig, use_container_width=True)
+
+        break
